51e1f0662d985dd0996e8bce665374141519abd4
BenjaminCoupry <56168210+BenjaminCoupry@users.noreply.github.com>
2020-09-22 17:56:59 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Hitoria
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AffichageTexte = new System.Windows.Forms.RichTextBox();
+            this.selecteurDir = new System.Windows.Forms.ComboBox();
+            this.bGO = new System.Windows.Forms.Button();
+            this.bretour = new System.Windows.Forms.Button();
+            this.bOuvrirRep = new System.Windows.Forms.Button();
+            this.bouvrirword = new System.Windows.Forms.Button();
+            this.bSave = new System.Windows.Forms.Button();
+            this.breload = new System.Windows.Forms.Button();
+            this.selecteurModeles = new System.Windows.Forms.ListBox();
+            this.indicPosition = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // AffichageTexte
+            // 
+            this.AffichageTexte.Location = new System.Drawing.Point(13, 68);
+            this.AffichageTexte.Name = "AffichageTexte";
+            this.AffichageTexte.Size = new System.Drawing.Size(775, 435);
+            this.AffichageTexte.TabIndex = 0;
+            this.AffichageTexte.Text = "Chargez un repertoire... Creez des modeles au format .rtf dans le dossier _modele" +
+    "s";
+            this.AffichageTexte.SelectionChanged += new System.EventHandler(this.AffichageTexte_SelectionChanged);
+            this.AffichageTexte.DoubleClick += new System.EventHandler(this.AffichageTexte_DoubleClick);
+            // 
+            // selecteurDir
+            // 
+            this.selecteurDir.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.selecteurDir.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.selecteurDir.FormattingEnabled = true;
+            this.selecteurDir.Location = new System.Drawing.Point(13, 509);
+            this.selecteurDir.Name = "selecteurDir";
+            this.selecteurDir.Size = new System.Drawing.Size(212, 21);
+            this.selecteurDir.TabIndex = 1;
+            this.selecteurDir.TextChanged += new System.EventHandler(this.comboBox1_TextChanged);
+            // 
+            // bGO
+            // 
+            this.bGO.Location = new System.Drawing.Point(232, 509);
+            this.bGO.Name = "bGO";
+            this.bGO.Size = new System.Drawing.Size(75, 23);
+            this.bGO.TabIndex = 2;
+            this.bGO.Text = "Creer";
+            this.bGO.UseVisualStyleBackColor = true;
+            this.bGO.Click += new System.EventHandler(this.bGO_Click);
+            // 
+            // bretour
+            // 
+            this.bretour.Location = new System.Drawing.Point(232, 538);
+            this.bretour.Name = "bretour";
+            this.bretour.Size = new System.Drawing.Size(75, 23);
+            this.bretour.TabIndex = 3;
+            this.bretour.Text = "Retour";
+            this.bretour.UseVisualStyleBackColor = true;
+            this.bretour.Click += new System.EventHandler(this.bretour_Click);
+            // 
+            // bOuvrirRep
+            // 
+            this.bOuvrirRep.Location = new System.Drawing.Point(13, 13);
+            this.bOuvrirRep.Name = "bOuvrirRep";
+            this.bOuvrirRep.Size = new System.Drawing.Size(107, 23);
+            this.bOuvrirRep.TabIndex = 6;
+            this.bOuvrirRep.Text = "Ouvrir Repertoire";
+            this.bOuvrirRep.UseVisualStyleBackColor = true;
+            this.bOuvrirRep.Click += new System.EventHandler(this.bOuvrirRep_Click);
+            // 
+            // bouvrirword
+            // 
+            this.bouvrirword.Location = new System.Drawing.Point(527, 12);
+            this.bouvrirword.Name = "bouvrirword";
+            this.bouvrirword.Size = new System.Drawing.Size(123, 23);
+            this.bouvrirword.TabIndex = 7;
+            this.bouvrirword.Text = "Ouvrir dans Word";
+            this.bouvrirword.UseVisualStyleBackColor = true;
+            this.bouvrirword.Click += new System.EventHandler(this.bouvrirword_Click);
+            // 
+            // bSave
+            // 
+            this.bSave.Location = new System.Drawing.Point(656, 12);
+            this.bSave.Name = "bSave";
+            this.bSave.Size = new System.Drawing.Size(75, 23);
+            this.bSave.TabIndex = 8;
+            this.bSave.Text = "Sauver";
+            this.bSave.UseVisualStyleBackColor = true;
+            this.bSave.Click += new System.EventHandler(this.bSave_Click);
+            // 
+            // breload
+            // 
+            this.breload.Location = new System.Drawing.Point(446, 13);
+            this.breload.Name = "breload";
+            this.breload.Size = new System.Drawing.Size(75, 23);
+            this.breload.TabIndex = 9;
+            this.breload.Text = "Recharger";
+            this.breload.UseVisualStyleBackColor = true;
+            this.breload.Click += new System.EventHandler(this.breload_Click);
+            // 
+            // selecteurModeles
+            // 
+            this.selecteurModeles.FormattingEnabled = true;
+            this.selecteurModeles.Location = new System.Drawing.Point(325, 509);
+            this.selecteurModeles.Name = "selecteurModeles";
+            this.selecteurModeles.Size = new System.Drawing.Size(208, 95);
+            this.selecteurModeles.TabIndex = 10;
+            // 
+            // indicPosition
+            // 
+            this.indicPosition.AutoSize = true;
+            this.indicPosition.Location = new System.Drawing.Point(12, 52);
+            this.indicPosition.Name = "indicPosition";
+            this.indicPosition.Size = new System.Drawing.Size(16, 13);
+            this.indicPosition.TabIndex = 11;
+            this.indicPosition.Text = "...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 610);
+            this.Controls.Add(this.indicPosition);
+            this.Controls.Add(this.selecteurModeles);
+            this.Controls.Add(this.breload);
+            this.Controls.Add(this.bSave);
+            this.Controls.Add(this.bouvrirword);
+            this.Controls.Add(this.bOuvrirRep);
+            this.Controls.Add(this.bretour);
+            this.Controls.Add(this.bGO);
+            this.Controls.Add(this.selecteurDir);
+            this.Controls.Add(this.AffichageTexte);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox AffichageTexte;
+        private System.Windows.Forms.ComboBox selecteurDir;
+        private System.Windows.Forms.Button bGO;
+        private System.Windows.Forms.Button bretour;
+        private System.Windows.Forms.Button bOuvrirRep;
+        private System.Windows.Forms.Button bouvrirword;
+        private System.Windows.Forms.Button bSave;
+        private System.Windows.Forms.Button breload;
+        private System.Windows.Forms.ListBox selecteurModeles;
+        private System.Windows.Forms.Label indicPosition;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace Hitoria
+{
+    public partial class Form1 : Form
+    {
+        string DirBase = "";
+        string PathActuel = "";
+        List<string> Historique = new List<string>();
+        Dictionary<string, string> Elements = new Dictionary<string, string>();
+        Dictionary<string, string> Modeles = new Dictionary<string, string>();
+        
+        public Form1()
+        {
+            InitializeComponent();
+            bGO.Enabled = false;
+            freeze(true);
+            
+        }
+
+        private void SetModeles()
+        {
+            selecteurModeles.Items.Clear();
+            foreach(string st in Modeles.Keys)
+            {
+                selecteurModeles.Items.Add(st);
+            }
+            selecteurModeles.SelectedIndex = 0;
+        }
+        private void SetElements()
+        {
+            selecteurDir.Items.Clear();
+            foreach (string st in Elements.Keys)
+            {
+                selecteurDir.Items.Add(st);
+            }
+            
+        }
+
+        private void freeze(bool etat)
+        {
+            breload.Enabled = !etat;
+            bouvrirword.Enabled = !etat;
+            bSave.Enabled = !etat;
+            bGO.Enabled = !etat;
+            bretour.Enabled = !etat;
+        }
+
+        private void ouvrirRep(string Pth)
+        {
+            DirBase = "";
+            PathActuel = "";
+            Historique = new List<string>();
+            Elements = new Dictionary<string, string>();
+            Modeles = new Dictionary<string, string>();
+
+            DirBase = Pth;
+            Console.WriteLine(DirBase);
+            if (!System.IO.Directory.Exists(DirBase + "/_modeles"))
+            {
+                System.IO.Directory.CreateDirectory(DirBase + "/_modeles");
+                if (!System.IO.File.Exists(DirBase + "/start.rtf"))
+                {
+                    using (RichTextBox RTB = new RichTextBox())
+                    {
+                        RTB.Text = "";
+                        RTB.SaveFile(DirBase + "/start.rtf", RichTextBoxStreamType.RichText);
+                    }
+                }
+                if (!System.IO.File.Exists(DirBase + "/M0"))
+                {
+                    System.IO.Directory.CreateDirectory(DirBase + "/M0");
+                }
+                if (!System.IO.File.Exists(DirBase + "/_modeles/M0.rtf"))
+                {
+                    using (RichTextBox RTB = new RichTextBox())
+                    {
+                        RTB.Text = "";
+                        RTB.SaveFile(DirBase + "/_modeles/M0.rtf", RichTextBoxStreamType.RichText);
+                    }
+                }
+            }
+            PathActuel = DirBase + "/start.rtf";
+            string[] modeles = System.IO.Directory.GetFiles(DirBase + "/_modeles");
+            foreach (string md in modeles)
+            {
+                Modeles.Add(System.IO.Path.GetFileNameWithoutExtension(md), md);
+            }
+            foreach (string pm in System.IO.Directory.GetDirectories(DirBase))
+            {
+                string dn = new System.IO.DirectoryInfo(pm).Name;
+                if (!(dn[0] == '_'))
+                {
+                    string[] files = System.IO.Directory.GetFiles(pm);
+                    foreach (string stf in files)
+                    {
+                        if (new System.IO.FileInfo(stf).Extension == ".rtf")
+                        {
+                            Elements.Add(System.IO.Path.GetFileNameWithoutExtension(stf), stf);
+                        }
+                    }
+                }
+            }
+            Elements.Add("start", DirBase + "/start.rtf");
+            AffichageTexte.LoadFile(PathActuel);
+            SetModeles();
+            SetElements();
+            indicPosition.Text = System.IO.Path.GetFileNameWithoutExtension(PathActuel);
+            freeze(false);
+        }
+
+        private void bOuvrirRep_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    ouvrirRep(fbd.SelectedPath);
+                }
+            }
+        }
+
+        private void bSave_Click(object sender, EventArgs e)
+        {
+            AffichageTexte.SaveFile(PathActuel);
+        }
+
+        private void bouvrirword_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(PathActuel);
+        }
+        private void reload()
+        {
+            string cdir = PathActuel;
+            ouvrirRep(DirBase);
+            PathActuel = cdir;
+            AffichageTexte.LoadFile(PathActuel);
+        }
+        private void breload_Click(object sender, EventArgs e)
+        {
+            reload();
+        }
+
+        private void comboBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (selecteurDir.Text =="")
+            {
+                bGO.Enabled = false;
+            }
+            else
+            {
+                
+                if (Elements.ContainsKey(selecteurDir.Text))
+                {
+                    bGO.Enabled = true;
+                    bGO.Text = "Aller";
+                }
+                else
+                {
+                    if(selecteurModeles.SelectedIndex ==-1)
+                    {
+                        bGO.Enabled = false;
+                    }
+                    else
+                    {
+                        bGO.Enabled = true;
+                    }
+                    bGO.Text = "Creer";
+                }
+            }
+        }
+
+        private void AffichageTexte_SelectionChanged(object sender, EventArgs e)
+        {
+            selecteurDir.Text = AffichageTexte.SelectedText.Trim();
+        }
+
+        private void Avancer()
+        {
+            if (selecteurDir.Text != "")
+            {
+                AffichageTexte.SaveFile(PathActuel);
+                string seltext = selecteurDir.Text.Trim();
+                Historique.Add(PathActuel);
+                if (Elements.ContainsKey(seltext))
+                {
+                    PathActuel = Elements[seltext];
+                    AffichageTexte.LoadFile(PathActuel);
+                }
+                else
+                {
+                    string name = seltext;
+                    string model = (string)selecteurModeles.SelectedItem;
+                    AffichageTexte.LoadFile(Modeles[model]);
+                    if (!System.IO.Directory.Exists(Modeles[model]))
+                    {
+                        System.IO.Directory.CreateDirectory(DirBase + "/" + model);
+                    }
+                    string pathsave = DirBase + "/" + model + "/" + name + ".rtf";
+                    PathActuel = pathsave;
+                    AffichageTexte.SaveFile(pathsave);
+                    Elements.Add(name, pathsave);
+                    SetElements();
+                }
+                indicPosition.Text = System.IO.Path.GetFileNameWithoutExtension(PathActuel);
+            }
+        }
+
+        private void bGO_Click(object sender, EventArgs e)
+        {
+            Avancer();
+        }
+
+        private void bretour_Click(object sender, EventArgs e)
+        {
+            if(Historique.Count != 0)
+            {
+                PathActuel = Historique[Historique.Count - 1];
+                AffichageTexte.LoadFile(PathActuel);
+                Historique.RemoveAt(Historique.Count - 1);
+                indicPosition.Text = System.IO.Path.GetFileNameWithoutExtension(PathActuel);
+            }
+        }
+
+        private void AffichageTexte_DoubleClick(object sender, EventArgs e)
+        {
+            Avancer();
+        }
+    }
+}
